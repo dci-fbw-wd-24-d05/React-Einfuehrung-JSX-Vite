3fbb3903aeaf52621abbbc704511c4ccf6f99bb0
rami-mohamad <rami_zezo1986@hotmail.com>
2025-01-08 12:18:15 +0100

    JSX tryed and explained with array and object

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const person = {
+    name: "John",
+    age: 30,
+  };
 
+  const users = [
+    {
+      name: "Anna",
+      age: 20,
+    },
+    {
+      name: "Sophie",
+      age: 40,
+    },
+  ];
+  // Return darf nur ein element zurueck geben, entweder fragments oder ein element unser wahl
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      {/* Fragments <></> */}
+      <h1>Try JSX</h1>
+      <p>name: {person.name}</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias
+        nobis, ea quam officia alias consectetur sed adipisci quaerat quisquam
+        voluptates eius minus nostrum ullam optio, quos eos tempora non enim?
       </p>
+
+      {users.map((user) => {
+        return (
+          <div key={user.age}>
+            <h2>{user.name}</h2>
+            <p>age: {user.age}</p>
+          </div>
+        );
+      })}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
