605ec0ff305e6814269d75b695e4ad19bdeedde1
rami-mohamad <rami_zezo1986@hotmail.com>
2025-01-09 09:25:11 +0100

    edited branches und comments in the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import seaDiving from "./assets/sea-diving.avif";
-
+import "./App.css";
 function App() {
   return (
-    <div>
+    <>
       <h1>Image trying</h1>
-      <img
-        src="https://images.unsplash.com/photo-1682687981630-cefe9cd73072?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="sea diving"
-      />
-      <img src={seaDiving} alt="" />
-    </div>
+      {/* Wenn das Image external ist, kann ein Link für das Image angewendet werden. */}
+      <div className="image">
+        <img
+          src="https://images.unsplash.com/photo-1682687981630-cefe9cd73072?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="sea diving"
+        />
+      </div>
+      {/* Wenn das Image lokal ist, muss es importiet werden */}
+      <div className="image">
+        <img src={seaDiving} alt="" />
+      </div>
+    </>
   );
 }
 
